c6cfa9060b8e6a56ab055da032309fb344fc51cf
Miriam-Hein <77849335+Miriam-Hein@users.noreply.github.com>
2021-03-07 21:04:04 +0000

    Week 06 Problem
    Squareroot.py Week 06 problem solution

---
+++ squareroot.py
@@ -0,0 +1,20 @@
+# squareroot.py
+# This program takes a positive floating-point number as input and outputs an approximation of its square root.
+# Author: Miriam Heinlein
+
+# User Input - positive number
+positiveNumber = input ("Please enter a positive number: ")
+
+# sqrt Function
+sqrt = 5
+
+#def sqrt(t):
+#        r = t/2.0; t = float(t)
+#       i = 0
+#       while i &lt; 10:
+#               r = (r+t/r)/2.0
+#               i += 1
+#       return r
+
+#Output 
+print ("The square root of", positiveNumber, "is approx.", sqrt)

680adaa5d4fdb7cd05fc0e3a3bc8cbd5c44c468a
Miriam-Hein <77849335+Miriam-Hein@users.noreply.github.com>
2021-03-08 18:00:41 +0000

    Week 06 Problem
    After researching the newton method on https://hackernoon.com/calculating-the-square-root-of-a-number-using-the-newton-raphson-method-a-how-to-guide-yr4e32zo and using the calculator.  Code found as per source of week 06 on the Readme file.

--- squareroot.py
+++ squareroot.py
@@ -1,20 +1,37 @@
 # squareroot.py
-# This program takes a positive floating-point number as input and outputs an approximation of its square root.
+# This program takes a positive floating-point number as input and return the approximate square root of a number using Newtons method  
 # Author: Miriam Heinlein
 
+#sqrt function
+def sqrt(n, l) : 
+
+    # Assuming the sqrt of n and x_i 
+    x = positiveNumber  
+  
+    # To count the number of iterations  
+    count = 0 
+  
+    while (1) :  
+        count += 1 
+  
+        # Calculate more closed x  
+        root = (x + (positiveNumber / x))/2  
+  
+        # Check for closeness  
+        if (abs(root - x) < l) : # if the absolute value(postive) of root minus the user's input is smaller than defined l (0.1) stop the loop and return output otherwise continue
+            break 
+  
+        # Update root  
+        x = root 
+  
+    return root
+   
+  
 # User Input - positive number
-positiveNumber = input ("Please enter a positive number: ")
+positiveNumber = float(input ("Please enter a positive number: "))
 
-# sqrt Function
-sqrt = 5
-
-#def sqrt(t):
-#        r = t/2.0; t = float(t)
-#       i = 0
-#       while i &lt; 10:
-#               r = (r+t/r)/2.0
-#               i += 1
-#       return r
+#defined when to stop approximity of square root, in this case once the value goes below 0.1 return output
+l = 0.10 
 
 #Output 
-print ("The square root of", positiveNumber, "is approx.", sqrt)
+print ("The square root of", positiveNumber, "is approx.", sqrt(positiveNumber,l))

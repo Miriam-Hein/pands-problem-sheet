a4102376d8e888459def2987a00004cb64cd7106
Miriam Heinlein <g00398281@gmit.ie>
2021-04-16 15:04:20 +0100

    comments added to Week 06 task

--- squareroot.py
+++ squareroot.py
@@ -9,13 +9,13 @@ n = float(input ("Please enter a positive number: "))
 def newtonSqrt(n): 
 
     # Setting variables
-    approx = 0.5 * n 
-    root = 0.5 * (approx+(n/approx))
+    approx = 0.5 * n # for example n = 10 --> 5
+    root = 0.5 * (approx+(n/approx)) # root with n=10 is 3.5
   
     while root !=approx: # Executes the while loop until the approximation is equal to the root 
         
-        approx = root 
-        root =  (approx +(n/approx))/2  # 0.5 * or /2 is the same
+        approx = root # every time approx will be changed to root, in example to 3.5 until finally the values align --> return
+        root =  (approx +(n/approx))/2  # 0.5 * or /2 is the same 
     return approx
 
 # Output returning approx. square root with one decimal digit
